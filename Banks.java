a43211db1c18bf24736b2d6e8c75abe4f19c6b52
Tommy Demehin <33219821+PlayerOne007@users.noreply.github.com>
2021-06-13 20:31:59 +0100

    Add files via upload
    New improvements made.

---
+++ Banks.java
@@ -0,0 +1,16 @@
+
+public class Banks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Account tom = new Account("Tommy Demehin ", "t007");
+	   
+	    
+        Account nwo = new Account("Kelly Roland", "rx9");
+	    
+	    nwo.showMenu();
+
+	}
+
+}
