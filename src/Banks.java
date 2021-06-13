abe30df97347f0504faa2ad2ffc0ba2df8727ad8
Tommy <cdemehin@gmail.com>
2021-06-13 20:28:20 +0100

    Improvements

--- src/Banks.java
+++ src/Banks.java
@@ -4,7 +4,7 @@ public class Banks {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Account tom = new Account("Tommy Demehin", "t007");
+		Account tom = new Account("Tommy Demehin ", "t007");
 	   
 	    
         Account nwo = new Account("Kelly Roland", "rx9");
